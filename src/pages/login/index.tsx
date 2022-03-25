94dd429df2cad0d9a8a59679119aac5507dfca2a
qanglee <545540710@qq.com>
2022-03-25 19:24:58 +0800

    feat: 请求接口，测试代理功能ok

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import styles from "./index.module.scss";
+import requset from '@COMMON/reuqest'
 import { Button, Layout, Card } from "antd";
 export default () => {
+  const fetchRegister =()=>{
+    requset.post('/api/users/register',{
+      username:1,
+      password:2
+    }).then((data:any)=>{
+      alert("ok")
+    })
+  }
   return (
     <Layout className={styles.layout}>
-        <Card >
-            <Button>登陆</Button>
-            <Button>注册</Button>
+        <Card className={styles.login_table}>
+          <Button>登陆</Button>
+          <Button onClick={()=>{
+            fetchRegister()
+          }}>注册</Button>
         </Card>
     </Layout>
   );
