231f6e10833230f044b859d128f639177d401c7a
qanglee <545540710@qq.com>
2022-03-29 16:39:44 +0800

    feat: test ci/cd

--- src/pages/microFrontends/index.tsx
+++ src/pages/microFrontends/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 export default () => {
   useEffect(() => {
-    //启动微前端 1
+    //启动微前端 2
     window.g_microAppsStart()
   }, [])
   return <div key="sub-View" id="sub-View"></div>
